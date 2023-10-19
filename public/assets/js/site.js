f9b1b4c992b5f22626e532d55af4913283945313
Shawn Girard <spgirard@comcast.net>
2023-10-18 21:10:24 -0400

    Add basic messages and form layout

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,1 +1,52 @@
 // SITE JAVASCRIPT
+
+let text = "Sed vitae sem quis mi euismod egestas lobortis vel lorem. Morbi conv."
+let textLength = text.length
+console.log(textLength)
+
+
+// store default phrases
+
+let lovePhrases = [
+    "Love conquers all.",
+    "Love at first sight.",
+    "Love is blind.",
+    "Love knows no bounds.",
+    "Love is a two-way street.",
+    "Love is a beautiful thing.",
+    "Love makes the world go 'round.",
+    "All you need is love.",
+    "Love is the key to happiness.",
+    "Love makes life worth living.",
+    "Love is the greatest gift of all.",
+    "Love is patient and kind.",
+    "Love is a powerful force.",
+    "Love is the language of the heart.",
+    "Love is like a fine wine.",
+    "Love is a journey, not a destination.",
+    "Love is in the air.",
+    "Love makes every day special.",
+    "Love makes you feel alive.",
+    "Love is a treasure.",
+    "Love is the music of the soul.",
+    "Love is a work of art.",
+    "Love is a sweet melody.",
+    "Love is a warm embrace.",
+    "Love is the answer to everything.",
+    "Love is the glue that binds us."
+  ];
+
+//   where the text shows
+let loveText = document.getElementById("loveText");
+getLoveText();
+
+let loveButton = document.getElementById("loveButton")
+loveButton.addEventListener('click', function() {
+    getLoveText()
+})
+
+function getLoveText() {
+    let randNum = Math.floor(Math.random() * lovePhrases.length);
+    loveText.textContent =  lovePhrases[randNum];
+}
+
--- reference/readme.txt
+++ reference/readme.txt
@@ -1,5 +1,12 @@
 Project notes, specifications, and/or reference material.
 
+Original globe approach:
+https://stackoverflow.com/questions/27781634/rotating-globe-in-css
+
+Star BG:
+
+<a href="https://www.freepik.com/free-photo/starry-night-sky_7061153.htm#query=star%20background&position=0&from_view=keyword&track=ais">Image by kjpargeter</a> on Freepik
+
 To deploy to github pages using subdirectory 'public':
     https://gist.github.com/cobyism/4730490
     git subtree push --prefix public origin gh-pages
